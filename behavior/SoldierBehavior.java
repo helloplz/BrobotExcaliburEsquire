b9741d22b85d44f59a702d057f3020435a034016
helloplz <helloplz@mit.edu>
2013-01-17 00:46:23 -0500

    Changed soldierBehavior

--- behavior/SoldierBehavior.java
+++ behavior/SoldierBehavior.java
@@ -2,14 +2,36 @@ package behavior;
 
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
+import behavior.RobotBehavior;
 
 public class SoldierBehavior extends RobotBehavior {
-  public void moveTo(RobotController rc, MapLocation loc) {
-    
-  }
+    public void moveTo(RobotController rc, MapLocation loc) {
+        
+        Direction roboDir = rc.getLocation().directionTo(loc);
+        Location mineLoc = loc.add(roboDir, 1);
+        Team mineTeam = rc.senseMine(mineLoc);
+        Location backUpLoc = rc.getLocation().
+        
+        if (mineTeam = rc.getTeam() || mineTeam = null){
+            isEnemyMine = false;
+        }
+        if (mineTeam != null && mineTeam != rc.getTeam()){
+            isEnemyMine = true;
+        }
+        
+        if(isEnemyMine){
+            rc.defuseMine(mineLoc));
+        }
+        if(rc.canMove(roboDir) && !isEnemyMine){
+            rc.move(roboDir);
+        }
+        else{
+            moveTo(rc,loc.add(0,1) );
+        }
+    }
 
-  @Override
-  public void run(RobotController rc) {
-    
-  }
+    @Override
+    public void run(RobotController rc) {
+
+    }
 }
