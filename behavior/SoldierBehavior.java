2a66172dabf6c149b50012d51de548711479d17c
Charlie Gordon <cwgordon7@cwgordon.com>
2013-01-17 01:00:19 -0500

    Fixed zachs code kinda well it compiles now at least.

--- behavior/SoldierBehavior.java
+++ behavior/SoldierBehavior.java
@@ -1,32 +1,45 @@
 package team238.behavior;
 
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
-import behavior.RobotBehavior;
+import battlecode.common.Team;
+import team238.behavior.RobotBehavior;
 
 public class SoldierBehavior extends RobotBehavior {
     public void moveTo(RobotController rc, MapLocation loc) {
         
         Direction roboDir = rc.getLocation().directionTo(loc);
-        Location mineLoc = loc.add(roboDir, 1);
+        MapLocation mineLoc = loc.add(roboDir, 1);
         Team mineTeam = rc.senseMine(mineLoc);
-        Location backUpLoc = rc.getLocation().
-        
-        if (mineTeam = rc.getTeam() || mineTeam = null){
-            isEnemyMine = false;
-        }
+        MapLocation backUpLoc = rc.getLocation();
+
+        boolean isEnemyMine = false;
         if (mineTeam != null && mineTeam != rc.getTeam()){
             isEnemyMine = true;
         }
         
-        if(isEnemyMine){
-            rc.defuseMine(mineLoc));
+        if (isEnemyMine) {
+            try {
+              rc.defuseMine(mineLoc);
+            }
+            catch (GameActionException e) {
+              // TODO Auto-generated catch block
+              e.printStackTrace();
+            }
         }
-        if(rc.canMove(roboDir) && !isEnemyMine){
-            rc.move(roboDir);
+        if (rc.canMove(roboDir) && !isEnemyMine) {
+            try {
+              rc.move(roboDir);
+            }
+            catch (GameActionException e) {
+              // TODO Auto-generated catch block
+              e.printStackTrace();
+            }
         }
-        else{
-            moveTo(rc,loc.add(0,1) );
+        else {
+            moveTo(rc, loc.add(0,1));
         }
     }
 
